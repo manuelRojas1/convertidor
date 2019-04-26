dceff8409fe6a59ed20b27fe139145a2338861f0
Paola <gborja311@gmail.com>
2019-04-26 00:24:44 -0500

    prueba

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-
-function calcula(operacion){
-    var operando1 = document.calc.operando1.value
-    var operando2 = document.calc.operando2.value 
-    var result = eval(operando1 + operacion + operando2)
-    document.calc.resultado.value = result 
-
-}
+console.log("hello world")
